b3331e653e7959eef07ae76aeffc5df042fb8c41
jp5201314 <1160873948@qq.com>
2017-04-05 15:15:52 +0800

    用户程序

---
+++ app/src/main/java/org/icot/icotpad/UserApplication.java
@@ -0,0 +1,19 @@
+package org.icot.icotpad;
+
+import android.app.Application;
+
+/**
+ * Created by 11608 on 2017/4/5.
+ */
+
+public class UserApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
