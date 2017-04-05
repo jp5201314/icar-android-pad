e5f396f9ffbe96096cffcffcade1676472df1bf5
ping.jiang <ping.jiang@icarowner.cn>
2017-04-05 14:28:08 +0800

    pad->登陆界面

---
+++ app/src/main/java/org/icot/icotpad/LoginActivity.java
@@ -0,0 +1,13 @@
+package org.icot.icotpad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
